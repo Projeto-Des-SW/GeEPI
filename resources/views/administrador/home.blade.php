b419ada65ec3308dc26d66f0576b2fee0d02d646
Douglas Filipe <batista.douglasfs@gmail.com>
2023-06-16 05:12:49 -0300

    View principal do Administrador quase finalizada, faltam alguns detalhes

--- resources/views/administrador/home.blade.php
+++ resources/views/administrador/home.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> Aqui é a página do administrador</h1>
+    <div class="container-fluid rounded-custom" style="background: white">
+        </br>
+        <div class="row">
+            <div class="col-md-3"> </div>
+
+            <div class="col-md-2">
+                <a>
+                    <img style="width: 100%" src="/images/home/solicitacoes_icon.svg">
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a>
+                    <img style="width: 100%" src="/images/home/gerenciar_estoque_icon.svg">
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a>
+                    <img style="width: 100%" src="/images/home/cadastrar_epi_icon.svg">
+                </a>
+            </div>
+        </div>
+
+        </br>
+
+        <div class="row">
+            <div class="col-md-4"> </div>
+
+            <div class="col-md-2">
+                <a>
+                    <img style="width: 100%" src="/images/home/relatorios_icon.svg">
+                </a>
+            </div>
+
+            <div class="col-md-2">
+                <a>
+                    <img style="width: 100%" src="/images/home/gerenciar_fiscais_icon.svg">
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
