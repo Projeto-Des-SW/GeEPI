5879184b6fcbf7684f58f669168264b6dd362ce9
douglasfsbatista <batista.douglasfs@gmail.com>
2023-06-16 17:24:45 -0300

    Correção na view home do Administrador

--- resources/views/administrador/home.blade.php
+++ resources/views/administrador/home.blade.php
@@ -4,27 +4,24 @@
     <div class="container rounded-5" style="background: white">
         </br>
         <div class="row">
-            <div class="col-md-1"> </div>
+            <div class="col-md-3"> </div>
 
             <div class="col-md-2">
                 <a>
-                    <img style="width: 125%" src="/images/home/solicitacoes_icon.svg">
+                    <img style="width: 100%" src="/images/home/solicitacoes_icon.svg">
                 </a>
             </div>
 
-            <div class="col-md-2"> </div>
-
             <div class="col-md-2">
                 <a>
-                    <img style="width: 125%" src="/images/home/gerenciar_estoque_icon.svg">
+                    <img style="width: 100%" src="/images/home/gerenciar_estoque_icon.svg">
                 </a>
             </div>
 
-            <div class="col-md-2"> </div>
 
             <div class="col-md-2">
                 <a>
-                    <img style="width: 125%" src="/images/home/cadastrar_epi_icon.svg">
+                    <img style="width: 100%" src="/images/home/cadastrar_epi_icon.svg">
                 </a>
             </div>
         </div>
@@ -32,21 +29,21 @@
         </br>
 
         <div class="row">
-            <div class="col-md-3"> </div>
+            <div class="col-md-4"> </div>
 
             <div class="col-md-2">
                 <a>
-                    <img style="width: 125%" src="/images/home/relatorios_icon.svg">
+                    <img style="width: 100%" src="/images/home/relatorios_icon.svg">
                 </a>
             </div>
 
-            <div class="col-md-2"> </div>
-
             <div class="col-md-2">
                 <a>
-                    <img style="width: 125%" src="/images/home/gerenciar_fiscais_icon.svg">
+                    <img style="width: 100%" src="/images/home/gerenciar_fiscais_icon.svg">
                 </a>
             </div>
         </div>
+
+        </br>
     </div>
 @endsection
