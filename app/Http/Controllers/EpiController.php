21834ec633c72ff6fafcc76c5e94014b4ceb8345
Douglas Filipe <batista.douglasfs@gmail.com>
2023-06-30 23:19:36 -0300

    CHORE: adição das rotas e funções do gerenciamento de epis

--- app/Http/Controllers/EpiController.php
+++ app/Http/Controllers/EpiController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEpiRequest;
+use App\Models\Epi;
+use App\Models\Estoque;
+use http\Message;
 use Illuminate\Http\Request;
 
 class EpiController extends Controller
@@ -12,7 +16,9 @@ class EpiController extends Controller
      */
     public function index()
     {
-        //
+        $epis = Epi::all()->sortBy('id');
+
+        return view('epi.index', compact('epis'));
     }
 
     /**
@@ -26,9 +32,11 @@ class EpiController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreEpiRequest $request)
     {
-        //
+        Epi::create($request->all());
+
+        return redirect(route('epi.index'))->with(['message' => 'EPI cadastrado com sucesso!']);
     }
 
     /**
@@ -42,24 +50,40 @@ class EpiController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($epi_id)
     {
-        //
+        $epi = Epi::findOrFail($epi_id);
+
+        return view('epi.edit', compact('epi'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreEpiRequest $request)
     {
-        //
+        $epi = Epi::findOrFail($request->epi_id);
+
+        $epi->nome = $request->nome;
+        $epi->quantidade_minima = $request->quantidade_minima;
+        $epi->certificado_aprovacao = $request->certificado_aprovacao;
+
+        $epi->update();
+
+        return redirect(route('epi.index'))->with(['message' => 'EPI atualizado com sucesso!']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function delete($epi_id)
     {
-        //
+        $epi = Epi::findOrFail($epi_id);
+        $estoque = Estoque::where('epi_id', $epi_id)->first();
+
+        $estoque->delete();
+        $epi->delete();
+
+        return redirect(route('epi.index'))->with(['message' => 'EPI excluído com sucesso!']);
     }
 }
