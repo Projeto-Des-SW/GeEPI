984bbbf7ca20ad4fdb77dfd5cdb715aa28ac6813
jvictorcordeiro <jvictorcor@hotmail.com>
2023-08-23 00:04:58 -0300

    adição do favicon e title no head das páginas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{ asset('/images/home/logo_geepi.svg') }}" type="image/svg+xml">
 
-        <title>Laravel</title>
+        <title>GeEPI</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
