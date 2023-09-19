1632c153501fc0b6c7605e158526623e50cd0d8d
Douglas Filipe <batista.douglasfs@gmail.com>
2023-09-19 00:30:02 -0300

    Envio de e-mail para o fiscal após uma solicitação ser realizada

---
+++ app/Mail/SolicitacaoRealizada.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SolicitacaoRealizada extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(private readonly array $data)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'GeEPI - Solicitação Recebida',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'email.solicitacao_realizada',
+            with: [
+                'adm'   => $this->data['adm'],
+                'fiscal' => $this->data['fiscal'],
+                'solicitacao' => $this->data['solicitacao'],
+                'imagem' => public_path('/images/relatorios/logo_geepi_dourado.png'),
+                'link' => 'http://http://127.0.0.1:8000/solicitacao/analisar',
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
